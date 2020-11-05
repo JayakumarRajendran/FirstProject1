415968c0aa775e30dff1d2920dc7a484d6abc934
JayakumarRajendran <jaiadheedhan@gmail.com>
2020-11-05 22:51:14 +0530

    Added Sample Java File
    This the example for the loop concept of the for loop

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Sample{
+public static void main(String arg[]){
+	System.out.println("--------------------------------------WElcome--------------------------------------");
+for(int i=1; i<=10: i++){
+	System.out.println("this is from the for loop ........ "+i);
+}
+	System.out.println("--------------------------------------End--------------------------------------");
+}
+}
